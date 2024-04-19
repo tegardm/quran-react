defcfdae79c1567f9bf90752e56f91301ad2316f
tegardm <rikutaru98@gmail.com>
2024-04-19 19:50:11 +0700

    Modify the home page

--- src/fragments/Home/HeroHome.jsx
+++ src/fragments/Home/HeroHome.jsx
@@ -1,4 +1,5 @@
 import Button from "../../elements/Button"
+import { Link } from "react-router-dom";
 
 const HeroHome = () => {
   return (
@@ -6,9 +7,9 @@ const HeroHome = () => {
     <div className="hero-content flex-col  lg:flex-row">
       <img src="/gambar.png" className="max-w-sm rounded-lg shadow-2xl" />
       <div className="ml-5">
-        <h1 className="text-5xl font-bold">Box Office News!</h1>
-        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <Button>Baca</Button>
+        <h1 className="text-5xl font-bold">Selamat Datang <strong>BacaQuran</strong></h1>
+        <p className="py-6">Website berisikan Qur'an dengan dilengkapi fitur live search dan terdapat audio media untuk setiap surah yang ada dan Doa secara Online Dan Gratis</p>
+        <Link to='/quran'><Button >Baca</Button></Link>
       </div>
     </div>
   </div>
